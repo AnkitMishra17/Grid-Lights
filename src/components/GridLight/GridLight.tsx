a18db22739eb8e422c83e9b39aa48440da1818eb
CodeSandbox <support@codesandbox.io>
2024-07-19 21:21:33 +0000

    Sandbox snapshot commit

--- src/components/GridLight/GridLight.tsx
+++ src/components/GridLight/GridLight.tsx
@@ -1,20 +1,35 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import "./GridLight.css";
 
 type numberArr = number[];
 function GridLight({ GRID_CONFIG }: { GRID_CONFIG: numberArr[] }) {
-  const [grid] = useState(GRID_CONFIG);
+  const [clickedElements, setClickedElements] = useState<numberArr[]>([]);
+  const getSquareCounts = useMemo(
+    () => GRID_CONFIG.flat().reduce((acc, ele) => acc + ele, 0),
+    [GRID_CONFIG]
+  );
+
+  console.log(getSquareCounts);
+
+  const handleClick = (rowIdx: number, colIdx: number) => {
+    if (clickedElements[rowIdx][colIdx]) {
+    }
+  };
   return (
     <div className="grid">
-      {grid &&
-        grid.map((ele: numberArr) => {
+      {GRID_CONFIG &&
+        GRID_CONFIG.map((ele: numberArr, rowIndex: number) => {
           return (
-            <div className="grid-row">
-              {ele.map((val: number) => {
+            <div className="grid-row" key={rowIndex}>
+              {ele.map((val: number, colIndex: number) => {
                 return val === 1 ? (
-                  <div className="grid-square"></div>
+                  <div
+                    className="grid-square"
+                    onClick={() => handleClick(rowIndex, colIndex)}
+                    key={colIndex}
+                  ></div>
                 ) : (
-                  <div className="hidden"></div>
+                  <div className="hidden" key={colIndex}></div>
                 );
               })}
             </div>
